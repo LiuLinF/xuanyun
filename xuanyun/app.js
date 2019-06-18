5e4c92c0d5d599c76b30cf14bb09906988bfc2e7
LiuLinF <2653854314@qq.com>
2019-06-18 21:05:39 +0800

    1.1.4

--- xuanyun/app.js
+++ xuanyun/app.js
@@ -2,10 +2,12 @@
 const express = require('express');
 const bodyParser = require('body-parser');
 /*引入路由模块*/
-
+var index=require("./routes/index");
 
 var app = express();
-var server = app.listen(3000);
+var server = app.listen(3000,function(){
+	console.log(`服务器创建成功!`)
+});
 //使用body-parser中间件
 app.use(bodyParser.urlencoded({
 	extended:false
@@ -13,5 +15,5 @@ app.use(bodyParser.urlencoded({
 //托管静态资源到public目录下
 app.use(express.static('public'));
 /*使用路由器来管理路由*/
-
+// app.use()
 
