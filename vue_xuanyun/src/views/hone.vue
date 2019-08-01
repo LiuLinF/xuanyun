bbfd6729690d918081f160ac93bb06b441ed0ad5
LiuLinF <2653854314@qq.com>
2019-08-01 20:53:57 +0800

    1.2.6

--- vue_xuanyun/src/views/hone.vue
+++ vue_xuanyun/src/views/hone.vue
@@ -1,7 +1,7 @@
 <template>
     <div>
         <my-header></my-header>
-        <router-view></router-view>>
+        <router-view></router-view>
         <my-footer></my-footer>
     </div>
 </template>
